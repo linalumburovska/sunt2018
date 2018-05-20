d6c56b1a2a7b6fb12799c2b7adbfafc2635fca5f
linalumburovska <linalumburovska20@gmail.com>
2018-05-20 16:48:00 +0200

    Added state and gave props to components; imgLeft and imgRight should probably be one lever lower - in the Home component

--- sunt-client/src/App.js
+++ sunt-client/src/App.js
@@ -1,34 +1,45 @@
 import React, {Component} from 'react';
-import './App.css';
 import {Link, Route, Switch, Redirect} from "react-router-dom";
 import {ProjectPagination} from "./ProjectPagination";
+import Gallery from "./pages/Gallery";
+import Home from "./pages/Home";
+import './App.css';
 
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <nav>
-                    <ul>
-                        <li><Link to={'/'}>Home</Link></li>
-                        <li><Link to={'/about'}>About</Link></li>
-                        <li><Link to={'/projects'}>Project</Link></li>
-                    </ul>
-                </nav>
-
-                <Switch>
-                    <Route exact path='/' component={Home}/>
-                    <Route path='/about' component={About}/>
-                    <Route path='/projects' component={ProjectPagination}/>
-                </Switch>
-            </div>
-        );
+  constructor() {
+    super();
+    this.state = {
+      imgLeft: '',
+      imgRight: '',
+      projects: []
     }
+  }
+
+
+  /*
+  * Get all projects and get images for home page
+  */
+  componentWillMount() {
+    ProjectAPI.all().then(data => this.setState({ projects: data, imgLeft: data[0].image[0].path, imgRight: data[1].image[0].path }));
+  }
+
+
+  render() {
+    return (
+      <div className="app">
+        <Switch>
+          <Route exact path='/' render={(props) => (<Home imgLeft={this.state.imgLeft} imgRight={this.state.imgRight} {...props} />)} />
+          <Route path='/about' component={About} />
+          <Route path='/projects' component={ProjectPagination} />
+          <Route path='/gallery' render={(props) => (<Gallery projects={this.state.projects} {...props} />)} />
+          <Redirect from="*" to="/" />
+        </Switch>
+      </div>
+    );
+  }
 }
 
-const Home = () => (
-    <p>Welcome Home</p>
-);
 
 const About = () => (
     <p>Modern Art stuff</p>
