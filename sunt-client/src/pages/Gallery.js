e7d7b666d315b60e67639d96190a98d368bfc933
linalumburovska <linalumburovska20@gmail.com>
2018-05-20 16:45:47 +0200

    Added Gallerycomponent

---
+++ sunt-client/src/pages/Gallery.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import {Router, Route, withRouter} from 'react-router';
+
+class Gallery extends Component {
+  constructor(props) {
+    super(props);
+
+    this.generateGallery = this.generateGallery.bind(this);
+    this.getAuthor = this.getAuthor.bind(this);
+  }
+
+
+  /*
+  * TBD: link to a given project
+  */
+  openProject(id) {
+    console.log("Open project ", id);
+  }
+
+
+  /*
+  * Get all authors for a given project
+  */
+  getAuthor(data) {
+    return data.map((author, key) => {
+      return <h2 key={key}>{author.name}</h2>
+    })
+  }
+
+
+  /*
+  * Generate gallery grid
+  */
+  generateGallery(data) {
+    return data.map((item, key) => {
+      return <figure key={key} className="wp-caption">
+         <img className="" src={''+item.image[0].path+''} />
+         <figcaption onClick={() => this.openProject(item.id)} className="wp-caption-text" ><div className="authors" >{this.getAuthor(item.author)}</div></figcaption>
+      </figure>
+    });
+  }
+
+
+  render() {
+    return (
+      <main className="main">
+        {this.generateGallery(this.props.projects)}
+      </main>
+    );
+  }
+}
+
+
+export default Gallery;
