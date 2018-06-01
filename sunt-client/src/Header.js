f74da46c97ba7881f67fd33b7e3044777bb87764
Nejc Hirci <nhirci@gmail.com>
2018-06-01 09:01:57 +0200

    Changed to Header from App. Ready to merge with home and gallery

---
+++ sunt-client/src/Header.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import './App.css';
+import {Link, Route, Switch, Redirect} from "react-router-dom";
+import {Project} from "./Project";
+import {ProjectPagination} from "./ProjectPagination";
+import {ProjectPresentation} from "./ProjectPresentation";
+
+
+class App extends Component {
+
+    render(){
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <div id="home">
+                        <HomeButton></HomeButton>
+                    </div>
+                    <div id="language">
+                        <LanguageButton></LanguageButton>
+                    </div>
+                    <div></div>
+                    <div id="about">
+                        <AboutButton></AboutButton>
+                    </div>
+                </div>
+                <div className="App-content">
+                    <Switch>
+                        <Route path='/projects/3/info'  component={ProjectPresentation}/>
+                        <Route path='/projects' component={ProjectPagination}/>
+                    </Switch>
+                </div>
+            </div>
+        );
+    }
+}
+const HomeButton = () => (
+    <Link to={'/'}>
+        <img src="http://localhost:3000/static_ikone/logo.png" alt="home"/>
+    </Link>
+);
+
+const LanguageButton = () => (
+    <Link to={'/'}>EN</Link>
+);
+
+const AboutButton = () => (
+    <Link to={'/projects'}>About</Link>
+
+);
+
+
+
+
+
+export default App;
