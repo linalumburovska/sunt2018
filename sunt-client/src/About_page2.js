a6c3176d19d9a6162375c65a8725dd28e7063bba
oblaksimon <so0098@student.uni-lj.si>
2018-06-19 23:20:45 +0200

    Add files via upload

---
+++ sunt-client/src/About_page2.js
@@ -0,0 +1,66 @@
+import React, {Component} from 'react';
+import './About.css';
+import {Link, Route, Switch, Redirect} from "react-router-dom";
+import {IndexConsumer} from "./IndexContext";
+
+let aboutDesc = "Salon ZDSLU 2017, osrednja selekcionirana letna razstava, ki jo organizira Zveza društev slovenskih likovnih umetnikov (ZDSLU), je v tokratni ediciji posvečena medijski umetnosti in nosi naslov Podoba v mediju, medij v kontekstu. Snovalec koncepta, skupaj z Janezom Strehovcem selektor in predsednik žirije Srečo Dragan je v besedilu za katalog med drugim zapisal: »Fokus tokratnega Salona je na prehodih slike, grafike, kipa in  fotografije v protokole medijske umetnosti. S poudarkom na uporabi konceptov novomedijske umetnosti v medprostorih, ki jih ti sprožajo z usodnimi preboji v posameznih fazah umetniškega raziskovanja. Ti ključni momenti se realizirajo v natisnjenih izjavah, postavitvah artefaktov v nov, drugačen kontekst, v beleženju sledi komuniciranja – dekodiranega branja in ozaveščanja, da obstaja skupno polje doživetja, tako za avtorja kot za gledalca/obiskovalca.« Razstavno postavitev je zasnoval na podlagi petih platform, ki kartirajo izbor projektov. Selektorja sta na razstavo uvrstila 27 projektov, komisija v sestavi Nicole Hewitt, Srečo Dragan in Janez Strehovec pa je podelila Glavno nagrado žirije Narviki Bovcon in Alešu Vaupotiču za delo 3D vizualizacija literarnih avtoric. Člani umetniških svetov regionalni društev likovnih umetnikov so podelili dve Priznanji Salona ZDSLU 2017 in sicer Gorazdu Krncu za delo Worth not knowing where Knowing is / Europe 2017 in Marku Glavaču za delo (pra)DIH – (digi) NEOKRNJENOST. Od edinega medijski umetnosti posvečenega Majskega salona ZDSLU pred letošnjim je minilo točno 20 let. Na Salonu 2017 je bila razstava z naslovom Modra roka iz leta";
+let aboutTitle = "Podoba v mediju medij v kontekstu";
+export class About extends React.Component {
+    render(){
+        const {back} = this.props.location.state;
+        console.log(this.props.location.state.back);
+        return (
+            <div className="About">
+                <div className="About-background-background">
+                    <div className="About-background">
+
+                        <img class="about-arrow about-arrow-left" src="http://localhost:3000/static_ikone/next.png" alt="next"/>
+                        <div className="about-scroll">
+                            <p class="about-naslov">SEZNAM UMETNIKOV</p>
+                            <div className="About-besedilo About-besedilo-levo">
+                                <p class="podnaslov">Umetniško raziskovanje-kot-performativne prakse:</p>
+                                <p class="navadno">Maja Smrekar</p>
+                                <p class="navadno">Marko Glavač</p>
+                                <p class="podnaslov">Podatkovne strukture-kot-novomedijski objekt:</p>
+                                <p class="navadno">Narvika Bovcon in Aleš Vaupotič</p>
+                                <p class="navadno">Franc Solina</p>
+                                <p class="navadno">Matija Jašarov</p>
+                                <p class="navadno">TNM - V.A.T. (Zoran Poznič, Maša Jazbec, Andrej Uduč)</p>
+                                <p class="navadno">Tilen Žbona</p>
+                                <p class="navadno">Peter Ciuha</p>
+                                <p class="navadno">Miran Kreš</p>
+                                <p class="navadno">Evelin Stermitz</p>
+                                <p class="podnaslov">Kontekstualni prostori-kot-video TV kolaž:</p>
+                                <p class="navadno about-bottom-space">Robert Černelč</p>
+                            </div>
+
+                            <div className="About-besedilo About-besedilo-desno">
+                                <p class="naslov"></p>
+                                <p class="podnaslov-zaceten-zamik">Skupno polje videa in slike:</p>
+                                <p class="navadno">Dominik Olmiah Križan</p>
+                                <p class="navadno">Aleš Sedmak</p>
+                                <p class="navadno">Črtomir Frelih</p>
+                                <p class="navadno">Dorian Španzl</p>
+                                <p class="navadno">Duša Jesih</p>
+                                <p class="navadno">Zoran Srdić Janežič</p>
+                                <p class="navadno">Peter Marolt</p>
+                                <p class="podnaslov">Video dela-kot-dokumenti, stališča:</p>
+                                <p class="navadno">Tomaž Furlan</p>
+                                <p class="navadno">Vanja Mervič</p>
+                                <p class="navadno">Vesna Čadež</p>
+                                <p class="navadno">Jure Fingušt Prebil</p>
+                                <p class="navadno">Eva Petrič</p>
+                            </div>
+
+                        </div>
+
+                        <img class="about-arrow about-arrow-right" src="http://localhost:3000/static_ikone/next.png" alt="next"/>
+
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
