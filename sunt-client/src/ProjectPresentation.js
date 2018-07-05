1b7b80a8c67e1b0cd18d9649a40f88b23452f762
evitapodobnikar <37217939+evitapodobnikar@users.noreply.github.com>
2018-07-05 13:45:42 +0200

    Add files via upload

--- sunt-client/src/ProjectPresentation.js
+++ sunt-client/src/ProjectPresentation.js
@@ -20,14 +20,17 @@ export class ProjectPresentation extends React.Component {
 
         };
 
+        console.log("Predstavi se", this.props);
+
     }
 
     componentDidMount() {
-        let index = parseInt(this.props.match.params.id, 10);
+        let index = parseInt(this.props.match.params.index, 10);
         console.log("INDEX", index);
 
         ProjectAPI.get(index)
             .then(item => {
+                console.log("KAJ DOBIMO", item);
                 this.setState({
                     isLoading: false,
                     images: item.image,
@@ -38,6 +41,7 @@ export class ProjectPresentation extends React.Component {
 
         AuthorAPI.get(index)
             .then(item => {
+            console.log(item);
             this.setState({
                 authors: item.name
             })
@@ -47,83 +51,69 @@ export class ProjectPresentation extends React.Component {
     }
 
     render() {
-        const {isLoading, title, description, authors, images} = this.state;
-        let id
+        const {isLoading, title, description, authors} = this.state;
 
         //const path = this.state.images[0].path;
         //const author = this.state.authors[0].name;
 
         const inputStyle = {
-            transform: 'rotate(180deg)',
+            transform:'rotate(180deg)',
         };
 
-        if (images !== undefined && images[0] !== undefined) {
-            return (
-                <IndexConsumer>
-                    {({index}) => {
-                        if (index.value !== undefined) {
-                            id = index.value;
-                        } else {
-                            id = parseInt(this.props.match.params.id, 10);
-                        }
-                        return (
-                            <div className="ProjectPresentation">
-                                <div className="Present-background"><img id="back" src={images[0].path}
-                                                                         alt={images[0].alt}/></div>
-                                <div id="content">
-                                    <div id="description">
-                                        <h1 className="title">{title}</h1>
-                                        <h2 className="author">{authors}</h2>
-                                        <h3 className="type">Digitalni print in video 2015-2017</h3>
-                                        <div className="text">
-                                            <p>{description}</p>
-                                            <br></br>
-                                            <p>Gre za večletni projekt, za katerega je avtoricadobila nagrado iz
-                                                prešernovega
-                                                sklada. Projekt je zelo dobro premišljen in kaže na možni razvoj ženske
-                                                skozi
-                                                evolucijo preko psa v svobodno bitje. Ženska ima
-                                                svobodo se odločiti, s komi in s čim bo nadaljevala vrsto človeka. Gre
-                                                za tipično
-                                                delo hibridne umetnosti, ki nakazuje sodelovanje z znanostjo.
-
-                                            </p>
-                                            <br></br>
-                                            <p>Maja SMREKAR (1978, Brežice)
-                                                Diplomirala na Akademiji za likovno umetnost in oblikovanje v Ljubljani
-                                                pri prof.
-                                                Jožetu Baršiju, smer kiparstvo, in pri somentorju prof. dr. Jožefu
-                                                Muhoviču ter
-                                                somentorici prof. Meti Hočevar (2006), kjer je na Oddelku za video in
-                                                nove medije
-                                                zaključila magistrski študij pri prof. Sreču Draganu (2016).</p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="ProjectPresentation-buttons">
-                                    <div id="Back" align="left"><BackButton index={id}
-                                                                            onClick={this.props.hide}></BackButton>
-                                    </div>
-                                    <div id="Media" align="right"><MediaButton></MediaButton></div>
-                                </div>
+        return (
+            <IndexConsumer>
+                {({index}) => {
+                    console.log("Present",this.state);
+                return(
+                <div className="ProjectPresentation">
+                    <div className="Present-background"><img id="back" src={"/1_maja_smrekar/_SIPK-8941.jpg"}
+                                                             alt="Štiri pravokotne visoke vitrine. V vsaki je majhen…uje statistiko o slovenskih literarnih avtoricah."/>
+                    </div>
+                    <div id="content">
+                        <div id="description">
+                            <h1 className="ProjectPresentation-title">{title}</h1>
+                            <h2 className="ProjectPresentation-author">{authors}</h2>
+                            <h3 className="ProjectPresentation-type">Digitalni print in video</h3>
+							<h4 className="ProjectPresentation-year">2015-2017</h4>
+                            <div className="ProjectPresentation-whole-text">
+                                <p className="ProjectPresentation-text">{description}</p>
+                                <br></br>
+                                <p className="ProjectPresentation-text">Gre za večletni projekt, za katerega je avtorica dobila nagrado iz prešernovega
+                                    sklada. Projekt je zelo dobro premišljen in kaže na možni razvoj ženske skozi
+                                    evolucijo preko psa v svobodno bitje. Ženska ima
+                                    svobodo se odločiti, s komi in s čim bo nadaljevala vrsto človeka. Gre za tipično
+                                    delo hibridne umetnosti, ki nakazuje sodelovanje z znanostjo.
+                                </p>
+                                <br></br>
+                                <p className="ProjectPresentation-text">Maja SMREKAR (1978, Brežice)
+                                    Diplomirala na Akademiji za likovno umetnost in oblikovanje v Ljubljani pri prof.
+                                    Jožetu Baršiju, smer kiparstvo, in pri somentorju prof. dr. Jožefu Muhoviču ter
+                                    somentorici prof. Meti Hočevar (2006), kjer je na Oddelku za video in nove medije
+                                    zaključila magistrski študij pri prof. Sreču Draganu (2016).</p>
                             </div>
-                        );
-                    }}
-                </IndexConsumer>
-            )
-        }else{return(<div></div>)}
+                        </div>
+                    </div>
+                    <div className="ProjectPresentation-buttons">
+                        <div id="Back" align="left"><BackButton index={index} onClick={this.props.hide}></BackButton></div>
+                        <div id="Media" align="right"><MediaButton></MediaButton></div>
+                    </div>
+                </div>
+                );
+            }}
+            </IndexConsumer>
+        )
     }
 }
 function BackButton(props) {
     const {index, onClick} = props;
     return (
-        <Link to={`/projects/${index}`} onClick={onClick}>
+        <Link to={`/projects/${index.value}`} onClick={onClick}>
             <img src={"/static_ikone/next.png"} alt={"nazaj"} style={{transform: 'rotate(180deg)'}}/>
         </Link>
     )
 }
 
-function MediaButton(props) {
+const MediaButton = () => {
     return(
     <Link to="/media"><img src={"/static_ikone/kamera.png"} alt="medija"/></Link>);
 };
